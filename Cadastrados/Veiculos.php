0737cd0774473fe949bbbb0eef7707f21f5a9f08
Matheus <matheusoficial1411@gmail.com>
2023-12-01 12:16:26 -0300

    front.Cadastrados.iniciada

---
+++ Cadastrados/Veiculos.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Veiculos</title>
+    <link rel="stylesheet" href="../index.css">
+    <link rel="stylesheet" href="Cadastrados.css">
+</head>
+<body>
+    veiculos
+</body>
+</html>
---
+++ Cadastrados/alunosAceitos.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aceitos</title>
+    <link rel="stylesheet" href="../index.css">
+    <link rel="stylesheet" href="Cadastrados.css">
+</head>
+<body>
+    Alunos
+</body>
+</html>
