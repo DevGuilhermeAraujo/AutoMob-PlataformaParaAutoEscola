b058177284f13386b313072e9a22a92cedde1bcc
GabrielGarcia10 <garcia.gabrielrodrigues1@gmail.com>
2023-12-04 16:52:55 -0300

    Update banco de dados

--- bancoDeDados/autoescola.sql
+++ bancoDeDados/autoescola.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04/12/2023 às 19:27
+-- Tempo de geração: 04/12/2023 às 20:52
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -37,16 +37,6 @@ CREATE TABLE `agendamentos` (
   `data_aula` date NOT NULL,
   `horario_aula` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `agendamentos`
---
-
-INSERT INTO `agendamentos` (`id`, `aluno_id`, `professor_id`, `carro_id`, `data_aula`, `horario_aula`) VALUES
-(1, 1, 1, 1, '2023-10-10', '08:00:00'),
-(2, 2, 2, 2, '2023-10-10', '10:30:00'),
-(3, 3, 3, 3, '2023-10-10', '13:00:00'),
-(4, 4, 4, 4, '2023-10-10', '15:30:00');
 
 -- --------------------------------------------------------
 
@@ -89,28 +79,30 @@ CREATE TABLE `log_professores_carros` (
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `usuariosnovalids`
+-- Estrutura para tabela `tipos`
 --
 
-CREATE TABLE `usuariosnovalids` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `cpf` varchar(14) NOT NULL,
-  `data_nascimento` date NOT NULL,
-  `endereco` varchar(255) NOT NULL,
-  `telefone` varchar(15) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `tipo` int(1) NOT NULL,
-  `senha` varchar(60) NOT NULL
+CREATE TABLE `tipos` (
+  `id` int(1) NOT NULL,
+  `tipoNome` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tipos`
+--
+
+INSERT INTO `tipos` (`id`, `tipoNome`) VALUES
+(1, 'Gerente'),
+(2, 'Instrutor'),
+(3, 'Aluno');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `usuariosvalids`
+-- Estrutura para tabela `usuarios`
 --
 
-CREATE TABLE `usuariosvalids` (
+CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL,
   `cpf` varchar(14) NOT NULL,
@@ -118,23 +110,22 @@ CREATE TABLE `usuariosvalids` (
   `endereco` varchar(255) NOT NULL,
   `telefone` varchar(15) NOT NULL,
   `email` varchar(50) NOT NULL,
+  `senha` varchar(60) NOT NULL,
   `tipo` int(1) NOT NULL,
-  `senha` varchar(60) NOT NULL
+  `validado` int(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `usuariosvalids`
+-- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuariosvalids` (`id`, `nome`, `cpf`, `data_nascimento`, `endereco`, `telefone`, `email`, `tipo`, `senha`) VALUES
-(1, 'Maria Silva', '123.456.789-01', '1990-05-15', 'Rua A, 123', '(11) 98765-4321', 'maria@email.com', 2, ''),
-(2, 'João Oliveira', '987.654.321-09', '1985-08-20', 'Avenida B, 456', '(22) 98765-1234', 'joao@email.com', 2, ''),
-(3, 'Ana Souza', '555.555.555-55', '1995-02-10', 'Rua C, 789', '(33) 98765-5678', 'ana@email.com', 2, ''),
-(4, 'Pedro Santos', '111.222.333-44', '1980-11-25', 'Avenida D, 987', '(44) 98765-8765', 'pedro@email.com', 2, ''),
-(5, 'Camila Lima', '777.888.999-00', '1998-07-05', 'Rua E, 654', '(55) 98765-4321', 'camila@email.com', 2, ''),
-(6, 'Carlos Silva', '111.222.333-44', '1975-03-10', 'Rua X, 123', '(11) 98765-4321', 'carlos@email.com', 1, ''),
-(7, 'Fernanda Oliveira', '555.666.777-88', '1982-06-15', 'Avenida Y, 456', '(22) 98765-1234', 'fernanda@email.com', 1, ''),
-(8, 'Mariana Souza', '999.888.777-66', '1970-11-25', 'Rua Z, 789', '(33) 98765-5678', 'mariana@email.com', 1, '');
+INSERT INTO `usuarios` (`id`, `nome`, `cpf`, `data_nascimento`, `endereco`, `telefone`, `email`, `senha`, `tipo`, `validado`) VALUES
+(1, 'Maria Silva', '123.456.789-01', '1990-05-15', 'Rua A, 123', '(11) 98765-4321', 'maria@email.com', '', 3, 0),
+(2, 'João Oliveira', '987.654.321-09', '1985-08-20', 'Avenida B, 456', '(22) 98765-1234', 'joao@email.com', '', 3, 0),
+(3, 'Ana Souza', '555.555.555-55', '1995-02-10', 'Rua C, 789', '(33) 98765-5678', 'ana@email.com', '', 3, 0),
+(4, 'Pedro Santos', '111.222.333-44', '1980-11-25', 'Avenida D, 987', '(44) 98765-8765', 'pedro@email.com', '', 3, 0),
+(5, 'Camila Lima', '777.888.999-00', '1998-07-05', 'Rua E, 654', '(55) 98765-4321', 'camila@email.com', '', 3, 0),
+(6, 'Carlos Silva', '111.222.333-44', '1975-03-10', 'Rua X, 123', '(11) 98765-4321', 'carlos@email.com', '', 2, 0);
 
 -- --------------------------------------------------------
 
@@ -150,8 +141,9 @@ CREATE TABLE `view_alunos` (
 ,`endereco` varchar(255)
 ,`telefone` varchar(15)
 ,`email` varchar(50)
-,`tipo` int(1)
 ,`senha` varchar(60)
+,`tipo` int(1)
+,`validado` int(1)
 );
 
 -- --------------------------------------------------------
@@ -168,8 +160,9 @@ CREATE TABLE `view_professores` (
 ,`endereco` varchar(255)
 ,`telefone` varchar(15)
 ,`email` varchar(50)
-,`tipo` int(1)
 ,`senha` varchar(60)
+,`tipo` int(1)
+,`validado` int(1)
 );
 
 -- --------------------------------------------------------
@@ -179,7 +172,7 @@ CREATE TABLE `view_professores` (
 --
 DROP TABLE IF EXISTS `view_alunos`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_alunos`  AS   (select `usuariosvalids`.`id` AS `id`,`usuariosvalids`.`nome` AS `nome`,`usuariosvalids`.`cpf` AS `cpf`,`usuariosvalids`.`data_nascimento` AS `data_nascimento`,`usuariosvalids`.`endereco` AS `endereco`,`usuariosvalids`.`telefone` AS `telefone`,`usuariosvalids`.`email` AS `email`,`usuariosvalids`.`tipo` AS `tipo`,`usuariosvalids`.`senha` AS `senha` from `usuariosvalids` where `usuariosvalids`.`tipo` = 2)  ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_alunos`  AS   (select `usuarios`.`id` AS `id`,`usuarios`.`nome` AS `nome`,`usuarios`.`cpf` AS `cpf`,`usuarios`.`data_nascimento` AS `data_nascimento`,`usuarios`.`endereco` AS `endereco`,`usuarios`.`telefone` AS `telefone`,`usuarios`.`email` AS `email`,`usuarios`.`senha` AS `senha`,`usuarios`.`tipo` AS `tipo`,`usuarios`.`validado` AS `validado` from `usuarios` where `usuarios`.`tipo` = 2)  ;
 
 -- --------------------------------------------------------
 
@@ -188,7 +181,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `view_professores`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_professores`  AS   (select `usuariosvalids`.`id` AS `id`,`usuariosvalids`.`nome` AS `nome`,`usuariosvalids`.`cpf` AS `cpf`,`usuariosvalids`.`data_nascimento` AS `data_nascimento`,`usuariosvalids`.`endereco` AS `endereco`,`usuariosvalids`.`telefone` AS `telefone`,`usuariosvalids`.`email` AS `email`,`usuariosvalids`.`tipo` AS `tipo`,`usuariosvalids`.`senha` AS `senha` from `usuariosvalids` where `usuariosvalids`.`tipo` = 1)  ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_professores`  AS   (select `usuarios`.`id` AS `id`,`usuarios`.`nome` AS `nome`,`usuarios`.`cpf` AS `cpf`,`usuarios`.`data_nascimento` AS `data_nascimento`,`usuarios`.`endereco` AS `endereco`,`usuarios`.`telefone` AS `telefone`,`usuarios`.`email` AS `email`,`usuarios`.`senha` AS `senha`,`usuarios`.`tipo` AS `tipo`,`usuarios`.`validado` AS `validado` from `usuarios` where `usuarios`.`tipo` = 1)  ;
 
 --
 -- Índices para tabelas despejadas
@@ -218,16 +211,17 @@ ALTER TABLE `log_professores_carros`
   ADD KEY `carro_id` (`carro_id`);
 
 --
--- Índices de tabela `usuariosnovalids`
+-- Índices de tabela `tipos`
 --
-ALTER TABLE `usuariosnovalids`
+ALTER TABLE `tipos`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `usuariosvalids`
+-- Índices de tabela `usuarios`
 --
-ALTER TABLE `usuariosvalids`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tipo` (`tipo`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
@@ -237,7 +231,7 @@ ALTER TABLE `usuariosvalids`
 -- AUTO_INCREMENT de tabela `agendamentos`
 --
 ALTER TABLE `agendamentos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `carros`
@@ -252,16 +246,16 @@ ALTER TABLE `log_professores_carros`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `usuariosnovalids`
+-- AUTO_INCREMENT de tabela `tipos`
 --
-ALTER TABLE `usuariosnovalids`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tipos`
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT de tabela `usuariosvalids`
+-- AUTO_INCREMENT de tabela `usuarios`
 --
-ALTER TABLE `usuariosvalids`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restrições para tabelas despejadas
@@ -271,16 +265,22 @@ ALTER TABLE `usuariosvalids`
 -- Restrições para tabelas `agendamentos`
 --
 ALTER TABLE `agendamentos`
-  ADD CONSTRAINT `agendamentos_ibfk_1` FOREIGN KEY (`aluno_id`) REFERENCES `usuariosvalids` (`id`),
-  ADD CONSTRAINT `agendamentos_ibfk_2` FOREIGN KEY (`professor_id`) REFERENCES `usuariosvalids` (`id`),
+  ADD CONSTRAINT `agendamentos_ibfk_1` FOREIGN KEY (`aluno_id`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `agendamentos_ibfk_2` FOREIGN KEY (`professor_id`) REFERENCES `usuarios` (`id`),
   ADD CONSTRAINT `agendamentos_ibfk_3` FOREIGN KEY (`carro_id`) REFERENCES `carros` (`id`);
 
 --
 -- Restrições para tabelas `log_professores_carros`
 --
 ALTER TABLE `log_professores_carros`
-  ADD CONSTRAINT `log_professores_carros_ibfk_1` FOREIGN KEY (`professor_id`) REFERENCES `usuariosvalids` (`id`),
+  ADD CONSTRAINT `log_professores_carros_ibfk_1` FOREIGN KEY (`professor_id`) REFERENCES `usuarios` (`id`),
   ADD CONSTRAINT `log_professores_carros_ibfk_2` FOREIGN KEY (`carro_id`) REFERENCES `carros` (`id`);
+
+--
+-- Restrições para tabelas `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`tipo`) REFERENCES `tipos` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
