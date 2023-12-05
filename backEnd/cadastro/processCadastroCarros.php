e433ecb44e540f784a64f2e8e6081a9694421988
Guilherme_Araujo <tecgui.informatica@gmail.com>
2023-12-05 16:07:36 -0300

    finalizacoes

--- backEnd/cadastro/processCadastroCarros.php
+++ backEnd/cadastro/processCadastroCarros.php
@@ -1,18 +1,21 @@
 <?php
-include_once "../conexao.php";
-$db = new Conexao();
+include_once "../sessao.php";
 if (isset($_GET['cadastroCarro'])) {
     $marca = $_POST['marca'];
     $modelo = $_POST['modelo'];
     $ano = $_POST['ano'];
     $placa = $_POST['placa'];
     $capacidade = $_POST['capacidade'];
-    $db->executar("INSERT INTO carros(marca, modelo, ano, placa, capacidade_passageiros) VALUES('$marca', '$modelo', '$ano', '$placa', '$capacidade')");
-    $result = $db->executar("SELECT * FROM carros WHERE placa = '$placa'", true);
+    $idInstrutor = getId();
+    getDb()->executar("INSERT INTO carros(marca, modelo, ano, placa, capacidade_passageiros) VALUES('$marca', '$modelo', '$ano', '$placa', '$capacidade')");
+    $result = getDb()->executar("SELECT id FROM carros WHERE placa = '$placa'");
+    $idCarro = $result[0][0];
+    $result = getDb()->executar("SELECT id FROM carros WHERE placa = '$placa'", true);
     if ($result->rowCount() == 0) {
         header("Location: ../../Instrutor/homeInstrutor.php?cadCarFailed");
         exit();
     } else {
+        getDb()->executar("INSERT INTO log_instrutores_carros(Instrutor_id, carro_id) VALUES('$idInstrutor', '$idCarro')", true);
         header("Location: ../../Instrutor/homeInstrutor.php?cadCarSucess");
         exit();
     }
