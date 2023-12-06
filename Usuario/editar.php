4b7d361bc9af00269f96561314d358e3d0924ca3
Matheus <matheusoficial1411@gmail.com>
2023-12-06 14:06:47 -0300

    front.edit

--- Usuario/editar.php
+++ Usuario/editar.php
@@ -5,15 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar</title>
     <link rel="stylesheet" href="../index.css">
+    <link rel="stylesheet" href="Usuario.css">
 </head>
 <body>
 <div class="Inicio">
         <h1>Editar meu perfil</h1>
         <a href="homeUsuario.php">Voltar</a>
     </div>
-    <form>
-        <input type="E-mail">
-        <input type="text">
-    </form>
+   <div id="edt">
+        <img src="../Imgs/icoPerfil.png" alt="foto perfil">
+        <form>
+            <input type="E-mail" placeholder="E-mail ?">
+            <input type="text" placeholder="Telefone ?">
+            <input type="password" placeholder="Nova senha">
+            <input type="password" placeholder="Repita a nova senha">
+            <input type="password" placeholder="Digite a senha atual">
+            <input style="background-color:#02024b;color:white;border: 5px solid white" type="submit" value="Confirmar dados">
+        </form>
+   </div>
 </body>
 </html>
