df73b976568aae44197e967b7bacbc5f825cec5f
GabrielGarcia10 <garcia.gabrielrodrigues1@gmail.com>
2023-12-06 18:05:14 -0300

    Adicionado caixa de menssagem nos editar e fix processEditarUsuario

--- Instrutor/editar.php
+++ Instrutor/editar.php
@@ -23,6 +23,27 @@ if (isset($_GET['idCarro'])) {
     <link rel="stylesheet" href="../index.css">
     <link rel="stylesheet" href="Instrutor.css">
     <script src="../backEnd/script.js"></script>
+    <script>
+        var msg1;
+        function confirmMenssage(text) {
+            document.getElementsByName('marca')[0].setAttribute('value',document.getElementsByName('marca')[0].value);
+            document.getElementsByName('modelo')[0].setAttribute('value',document.getElementsByName('modelo')[0].value);
+            document.getElementsByName('ano')[0].setAttribute('value',document.getElementsByName('ano')[0].value);
+            document.getElementsByName('placa')[0].setAttribute('value',document.getElementsByName('placa')[0].value);
+            document.getElementsByName('capacidade')[0].setAttribute('value',document.getElementsByName('capacidade')[0].value);
+            msg1 = new MsgBox();
+            msg1.showInLine({
+                _idName: "msg22",
+                _type: msg1.SET_TYPE_TEXT,
+                _title: "Confirmação",
+                _menssagem: text,
+                _btnOkName: "Sim",
+                _btnOkAction: "document.querySelector('#formEdit').submit();",
+                _btnCancelName: "Não",
+                _autoDestroy: true
+            });
+        }
+        </script>
 </head>
 
 <body>
@@ -32,14 +53,14 @@ if (isset($_GET['idCarro'])) {
     </div>
     <div class="cad">
         <h2>Edite o veículo desejado</h2>
-        <form action="../backEnd/processEditarCarro.php?editarcarro" method="POST">
+        <form id="formEdit" action="../backEnd/processEditarCarro.php?editarcarro" method="POST" onsubmit="return (msg1.returnBtnClicked == MsgBox.BTN_OK)">
             <input type="text" name="id" value="<?= $idCarro ?>" readonly>
             <input type="text" name="marca" value="<?= $marca ?>" required>
             <input type="text" name="modelo" value="<?= $modelo ?>" required>
             <input type="text" name="ano" value="<?= $ano ?>" required>
             <input type="text" name="placa" value="<?= $placa ?>" required>
             <input type="text" name="capacidade" value="<?= $capacidade ?>" required>
-            <input id="cada" type="submit" value="Cadastrar">
+            <input id="cada" type="submit" onclick="confirmMenssage('Editar dados do veículo?');" value="Cadastrar">
         </form>
     </div>
 </body>
